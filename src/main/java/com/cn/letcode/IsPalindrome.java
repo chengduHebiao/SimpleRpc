4373f31fa4986ce73572bb145b94e4c35d66eda4
hebiao <hebiaoIT@163.com>
2019-06-19 16:07:39 +0800

    刷题

--- src/main/java/com/cn/letcode/IsPalindrome.java
+++ src/main/java/com/cn/letcode/IsPalindrome.java
@@ -5,8 +5,9 @@
 package com.cn.letcode;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -44,7 +45,7 @@ public class IsPalindrome {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+       /* System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(partition("aab"));
         Set<String> list = new HashSet<>();
         list.add("cats");
@@ -52,7 +53,10 @@ public class IsPalindrome {
         list.add("sand");
         list.add("and");
         list.add("cat");
-        System.out.println(wordBreak("catsandog", list));
+        System.out.println(wordBreak("catsandog", list));*/
+
+       //System.out.println(lengthOfLongestSubString("abc"));
+       System.out.println(convert("LEETCODEISHIRING",3));
     }
 
     public static int countSubstrings(String s) {
@@ -137,5 +141,73 @@ public class IsPalindrome {
         return canBreak[s.length()];
     }
 
+    private static boolean repeatChar(String chars){
+        StringBuilder stringBuilder = new StringBuilder();
+        for(Character c:chars.toCharArray()){
+            String str = stringBuilder.toString();
+            if(str.contains(c.toString())){
+                return true;
+            }
+             stringBuilder.append(c);
+        }
+        return false;
+    }
 
+    private static int lengthOfLongestSubString(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int end = 0, start = 0; end < n; end++) {
+            char alpha = s.charAt(end);
+            if (map.containsKey(alpha)) {
+                start = Math.max(map.get(alpha), start);
+            }
+            ans = Math.max(ans, end - start + 1);
+            map.put(s.charAt(end), end + 1);
+        }
+        return ans;
+    }
+
+    /**
+     * 将一个给定字符串根据给定的行数，以从上往下、从左到右进行 Z 字形排列。
+     *
+     * 比如输入字符串为 "LEETCODEISHIRING" 行数为 3 时，排列如下：
+     *
+     * L   C   I   R
+     * E T O E S I I G
+     * E   D   H   N
+     * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："LCIRETOESIIGEDHN"
+     * 整体的思路是遍历字符串，遍历过程中将每行都看成新的字符串构成字符串数组，最后再将该数组拼接起来即可
+     * 如果numRows=1则说明当前字符串即为结果，直接返回
+     * 否则整个字符串需要经历，向下向右，向下向右，这样的反复循环过程，设定down变量表示是否向下，loc变量表示当前字符串数组的下标
+     * 如果down为true，则loc+=1，字符串数组下标向后移动，将当前字符加入当前字符串中
+     * 如果down为false，则表示向右，则loc-=1，字符串数组下标向前移动，将当前字符加入当前字符串中
+     * 时间复杂度：O(n)，n为字符串s的长度
+     */
+    public static String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        int len = Math.min(s.length(), numRows);
+        String[] rows = new String[len];
+        for (int i = 0; i < len; i++) {
+            rows[i] = "";
+        }
+        int loc = 0;
+        boolean down = false;
+
+        for (int i = 0; i < s.length(); i++) {
+            rows[loc] += s.substring(i, i + 1);
+            if (loc == 0 || loc == numRows - 1) {
+                down = !down;
+            }
+            loc += down ? 1 : -1;
+        }
+
+        String ans = "";
+        for (String row : rows) {
+            ans += row;
+        }
+        return ans;
+    }
 }
