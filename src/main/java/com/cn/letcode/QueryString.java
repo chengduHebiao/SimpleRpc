9ef37cfe17d8fefbced2e4f767467e90089e4811
hebiao <hebiaoIT@163.com>
2019-06-26 14:00:02 +0800

    刷题

--- src/main/java/com/cn/letcode/QueryString.java
+++ src/main/java/com/cn/letcode/QueryString.java
@@ -84,30 +84,28 @@ public class QueryString {
 
     /**
      * 查找最长公共前缀
-     * @param strs
-     * @return
      */
     public static String longestCommonPrefix(String[] strs) {
         if (strs.length == 0 || strs[0] == null || strs[0].isEmpty()) {
             return "";
         }
-        if(strs.length ==1){
+        if (strs.length == 1) {
             return strs[0];
         }
-        int minLength =strs[0].length();
-        for(int temp=0;temp<strs.length;temp++){
-            minLength = Math.min(minLength,strs[temp].length());
+        int minLength = strs[0].length();
+        for (int temp = 0; temp < strs.length; temp++) {
+            minLength = Math.min(minLength, strs[temp].length());
         }
         String result = "";
         for (int j = 0; j < minLength; j++) {
-            String subString = strs[0].substring(0, j+1);
-            boolean ok =true;
+            String subString = strs[0].substring(0, j + 1);
+            boolean ok = true;
             for (int k = 1; k < strs.length; k++) {
-                if (!strs[k].substring(0,j+1).equals(subString)) {
-                   ok = false;
+                if (!strs[k].substring(0, j + 1).equals(subString)) {
+                    ok = false;
                 }
             }
-            if(ok){
+            if (ok) {
                 result = subString;
             }
 
@@ -116,9 +114,58 @@ public class QueryString {
 
     }
 
+    /**
+     * 电话号码的字母组合
+     *
+     */
+    public static List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if(null == digits || ""==digits){
+            return result;
+        }
+        Map<String, String> nums = new HashMap<String, String>() {
+            {
+                put("2", "abc");
+                put("3", "def");
+                put("4", "ghi");
+                put("5", "jkl");
+                put("6", "mno");
+                put("7", "pqrs");
+                put("8", "tuv");
+                put("9", "wxyz");
+            }
+        };
+        //回溯法
+        backtrack(nums, "", digits, result);
+        return result;
+    }
+
+    public static void backtrack(Map<String, String> phone, String combination, String next_digits,
+            List<String> output) {
+        // if there is no more digits to check
+        if (next_digits.length() == 0) {
+            // the combination is done
+            output.add(combination);
+        }
+        // if there are still digits to check
+        else {
+            // iterate over all letters which map
+            // the next available digit
+            String digit = next_digits.substring(0, 1);
+            String letters = phone.get(digit);
+            for (int i = 0; i < letters.length(); i++) {
+                String letter = phone.get(digit).substring(i, i + 1);
+                // append the current letter to the combination
+                // and proceed to the next digits
+                backtrack(phone, combination + letter, next_digits.substring(1), output);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
-       // roamanTOint("III");
-        String[] strings = new String[]{"c","acc","ccc"};
-        System.out.println(longestCommonPrefix(strings));
+        // roamanTOint("III");
+        String[] strings = new String[]{"c", "acc", "ccc"};
+        System.out.println(letterCombinations("237"));
     }
 }
